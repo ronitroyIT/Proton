1329da7a49c102e54416c33187795a9771c53980
Ronit Roy <ronitroytcs@gmail.com>
2025-04-29 18:50:24 +0530

    update gitignore and account sql file

--- src/main/resources/accountsdb.sql
+++ src/main/resources/accountsdb.sql
@@ -18,7 +18,10 @@
 --
 -- Table structure for table `role`
 --
-
+CREATE TABLE `ACCOUNT1` (
+  `Account_number` int(12) Not Null AUTO_INCREMENT,
+  `holder_name` varchar(40) default Null
+)
 DROP TABLE IF EXISTS `role`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
